a5b1a11c376c48f5227ba6ee0e7ee53819b1d6cf
ArnthorAtli <arnthoratli@gmail.com>
2025-09-05 13:39:18 +0000

    baetti vid lista af faedum

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,5 +21,40 @@ public class DemoApplication {
 
   // Katrín líka!
   // Silja líka! woohoo
+  public String[] faediOptions = {
+      "A1 - almennt fæði",
+      "A2 - Hentar eldri kynslóðinni",
+      "A3 - Grænmetisfæði",
+      "OP - Orku og próteinbætt fæði",
+      "RDS kjöt/fiskur",
+      "RDS grænmetisfæði",
+      "M1 - Mjúkt",
+      "M2 - Hakkað",
+      "M3 - Fínmaukað",
+      "F1 - Fljótandi fæði",
+      "F1 Sykurskert",
+      "F1 Mjólkurlaust",
+      "F2 - Tært fljótandi",
+      "F3 - Fljótandi fæði eftir aðgerð",
+      "F4 - Þykkfljótandi fæði",
+      "F4 Sykurskert",
+      "F5 - Fljótandi fæði kalt",
+      "Fituskert 40g",
+      "Próteinskert 50g",
+      "Próteinskert 50g Sykurskert",
+      "Próteinskert 60g",
+      "Saltskert",
+      "Salskert sykurskert",
+      "Blóðskilunarfæði",
+      "Blóðskilunarfæði Sykurskert",
+      "FSMS",
+      "Mjólkursykurskert",
+      "Glútensnautt",
+      "Örveruskert",
+      "Joðsnautt",
+      "Ungbarnafæði",
+      "FASTANDI",
+      "EINNOTA"
+  };
 
 }
