04e13d9d9604d3c9cbb00fed403ed62585a713c1
sunnakristin <125207743+sunnakristin@users.noreply.github.com>
2025-09-27 11:52:39 +0000

    set wards as REST api and can now call it successfully

--- src/main/java/is/hi/matarpontun/controller/WardController.java
+++ src/main/java/is/hi/matarpontun/controller/WardController.java
@@ -4,27 +4,33 @@ import is.hi.matarpontun.model.Ward;
 import is.hi.matarpontun.service.WardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 
-@Controller
+@RestController
+@RequestMapping("/wards")
 public class WardController {
 
     @Autowired
     private WardService wardService;
 
+    @PostMapping
     // This method takes simple data, calls the service, and returns a result.
-    public void createWard(String wardName, String password) {
+    public Ward createWard(@RequestParam String wardName, @RequestParam String password) {
         Ward newWard = new Ward(wardName, password);
-        wardService.createWard(newWard);
+        return wardService.createWard(newWard);
     }
 
+    @PostMapping("/signIn")
     // This method handles the sign-in logic.
-    public Optional<Ward> signIn(String wardName, String password) {
+    public Optional<Ward> signIn(@RequestParam String wardName, @RequestParam String password) {
         return wardService.signIn(wardName, password);
     }
+
+    @GetMapping
     // method to fetch all wards
     public List<Ward> fetchAllWards() {
         return wardService.findAllWards();
