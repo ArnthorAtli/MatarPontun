50d90e0757510fcde25bfab026850ec3f8429a0d
sunnakristin <125207743+sunnakristin@users.noreply.github.com>
2025-10-09 12:25:18 +0000

    POST /{id}/allergies/add -can now also add and create allergies for patients. Also will not duplicate if value already exists in the list

--- src/main/java/is/hi/matarpontun/service/PatientService.java
+++ src/main/java/is/hi/matarpontun/service/PatientService.java
@@ -20,12 +20,25 @@ public class PatientService {
         return patientRepository.findById(patientID);
     }
 
-    // Adds a single restriction string to the patient's restriction list. If the patient has no restriction yet, one is created automatically.
-    public Patient addRestriction(Long patientID, String restrictionText) {
+    // Adds a restriction string to the patient's restriction list. If the patient has no restriction yet, one is created automatically.
+    public Patient addRestriction(Long patientID, String restriction) {
         Patient patient = patientRepository.findById(patientID)
                 .orElseThrow(() -> new EntityNotFoundException("Patient not found"));
 
-        patient.getRestriction().add(restrictionText);
+        if (!patient.getRestriction().contains(restriction)) {
+            patient.getRestriction().add(restriction);
+        }
+        return patientRepository.save(patient);
+    }
+
+    // Adds an allergy string to the patient's allergy list. If the patient has no allergies yet, one is created automatically.
+    public Patient addAllergy(Long patientID, String allergy) {
+        Patient patient = patientRepository.findById(patientID)
+                .orElseThrow(() -> new EntityNotFoundException("Patient not found"));
+
+        if (!patient.getAllergies().contains(allergy)) {
+            patient.getAllergies().add(allergy);
+        }
         return patientRepository.save(patient);
     }
 }
