f701097b2d38a6c3bb9b780268857bbe3a8c2f10
Silja <sba31@hi.is>
2025-11-08 09:21:53 +0000

    það eru ehv comment sem má örgl taka út en þorði ekki alveg

--- src/main/java/is/hi/matarpontun/repository/RestrictionRepository.java
+++ src/main/java/is/hi/matarpontun/repository/RestrictionRepository.java
@@ -4,5 +4,4 @@ import is.hi.matarpontun.model.Restriction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestrictionRepository extends JpaRepository<Restriction, Long> {
-    //void save(Restriction restriction);
 }
